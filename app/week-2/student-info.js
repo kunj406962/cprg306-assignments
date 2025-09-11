b1d151efc193d2cd5dec80ca26e45eba90f8f6ed
Kunj Patel <pkunjk007@gmail.com>
2025-09-11 09:09:04 -0600

    added some tailwind changed the homepage and also added student-info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Student Info</h1>
+            <p>My Name: Kunj Patel</p>
+            <Link href="https://github.com/kunj406962/cprg306-assignments" className="hover:underline">This is my GitHub repository</Link>
+        </main>
+    )
+}
