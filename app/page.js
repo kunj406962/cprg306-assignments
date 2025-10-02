3d39d26fee1d6eb1a19759b32b739c3aad8b77c4
Kunj Patel <pkunjk007@gmail.com>
2025-10-02 10:02:39 -0600

    Made some changes to the main such that the whole li is a link

--- app/page.js
+++ app/page.js
@@ -1,24 +1,31 @@
 import Link from "next/link";
 export default function Page(){
-  const liCss="flex items-center px-4 py-3 rounded-lg bg-green-50 text-gray-700 hover:bg-green-100 transition-all transform hover:translate-x-4"
+  const linkCss="flex items-center px-4 py-3 rounded-lg bg-green-50 text-gray-700 hover:bg-green-100 transition-all transform hover:translate-x-4"
   const spanCss="w-2 h-2 bg-green-500 rounded-full mr-3"
+  const liCss="flex items-center"
 
   return(
     <main>
       <h1 className="text-2xl font-semibold text-gray-800 mb-4 flex items-center">CPRG 306: Web Development 2 - Assignments</h1>
       <ul className="space-y-3 w-max">
-        <li className={liCss}>
-          <span className={spanCss}></span>
-          <Link href="week-2" >This is the link to week-2 page</Link>
-        </li>
-        <li className={liCss}>
-          <span className={spanCss}></span>
-          <Link href="week-3" >This is the link to week-3 page</Link>
-        </li>
-        <li className={liCss}>
-          <span className={spanCss}></span>
-          <Link href="week-4" >This is the link to week-4 page</Link>
-        </li>
+        <Link href="week-2" className={linkCss}>
+          <li className={liCss}>
+            <span className={spanCss}></span>
+            <span>This is the link to week-2 page</span>
+          </li>
+        </Link>
+        <Link href="week-3" className={linkCss}>
+          <li className={liCss}>
+            <span className={spanCss}></span>
+            <span>This is the link to week-3 page</span>
+          </li>
+        </Link>
+        <Link href="week-4" className={linkCss}>
+          <li className={liCss}>
+            <span className={spanCss}></span>
+            <span>This is the link to week-4 page</span>
+          </li>
+        </Link>
       </ul>
     </main>
   )
